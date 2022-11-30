dc90976c56e1a94f59fb7b727e904fbb9d06e994
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-11-30 01:35:56 +0100

    Config for Geolatte

---
+++ src/main/java/com/example/parking/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package com.example.parking;
+
+import org.geolatte.geom.json.GeolatteGeomModule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    
+    @Bean
+    GeolatteGeomModule geolatteGeomModule() {
+        return new GeolatteGeomModule();
+    }
+
+}
