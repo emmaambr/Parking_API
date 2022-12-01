ad5ea44c9a6ae33880863d8dc9ae142a15078d59
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-12-02 00:53:16 +0100

    Add custom queries

--- src/main/java/com/example/parking/repository/ParkingEventRepository.java
+++ src/main/java/com/example/parking/repository/ParkingEventRepository.java
@@ -1,8 +1,20 @@
 package com.example.parking.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.example.parking.entity.ParkingEvent;
 
 public interface ParkingEventRepository extends CrudRepository<ParkingEvent, Long> {
     
+    @Query("""
+            SELECT p FROM ParkingEvent p WHERE (p.active) = true
+        """)  
+    List<ParkingEvent> filterActive();
+
+    @Query("""
+            SELECT p FROM ParkingEvent p WHERE (p.active) = false
+        """)
+    List<ParkingEvent> filterExpired();
 }
